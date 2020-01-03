b34f9c340cb367e6970a2fa5393a10a36814511f
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-01-03 01:38:01 +0000

    Automatically backed up by Learn

--- application.rb
+++ application.rb
@@ -4,7 +4,10 @@ class Application
     resp = Rack::Response.new
     resp.write "Hello, World"
     resp.finish
-  end
-
+      
+    end
+    def last_response
+      (Time.now.to_i % 2).zero? ?  ["Hello, World"] : ["Hello, my name is"] 
+    end
 end
 
